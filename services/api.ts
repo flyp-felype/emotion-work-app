283a43e3cc1db2bcd27a9b1ce6dacd2dbeb74674
Felype Fernandes <felypeweb@gmail.com>
2025-12-13 16:45:50 -0300

    Add API service with authentication and error handling
    - Introduced a new API service using Axios for making HTTP requests.
    - Implemented request interceptor to attach user token from SecureStore for authorization.
    - Added response interceptor to handle 401 errors by clearing user data and redirecting to the login screen.
    - Set a base URL and timeout for API requests to enhance reliability and performance.

---
+++ services/api.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { router } from "expo-router";
+import * as SecureStore from "expo-secure-store";
+
+const api = axios.create({
+  baseURL: "https://emotion-api-production-40d4.up.railway.app'",
+  timeout: 10000,
+});
+
+api.interceptors.request.use(
+  async (config: { headers: { Authorization: string } }) => {
+    try {
+      const token = await SecureStore.getItemAsync("user_token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } catch (error) {
+      console.error("Erro ao fazer requisição:", error);
+      router.push("/(auth)/login");
+    }
+
+    return config;
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      await SecureStore.deleteItemAsync("user_token");
+      await SecureStore.deleteItemAsync("user_profile");
+
+      router.replace("/(auth)/login");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
